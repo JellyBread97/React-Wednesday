2512f15e3a9ea6f3333e3ea2414dcbec685e87ab
MartinKotas <kotasmartin1997@gmail.com>
2022-11-30 15:45:00 +0000

    Netflix

---
+++ netflix/src/components/MovieGallery.jsx
@@ -0,0 +1,54 @@
+import { Component } from "react";
+import "./css/movie-gallery.css";
+import Spinner from "./Spinner";
+import { Row, Col } from "react-bootstrap";
+
+export default class MovieGallery extends Component {
+  state = {
+    gallery: [],
+    isFetching: true,
+  };
+
+  gallerySearch = this.props.gallerySearch ? this.props.gallerySearch : "arc";
+
+  fetchGallery = async () => {
+    setTimeout(async () => {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=2f95a32d&s=${this.gallerySearch}`
+      );
+
+      const data = await res.json();
+      this.setState({
+        gallery: data.Search,
+        isFetching: false,
+      });
+    }, 1000);
+  };
+
+  componentDidMount = () => {
+    this.fetchGallery();
+  };
+
+  render() {
+    const { gallery, isFetching } = this.state;
+    return (
+      <>
+        <h2 className="text-capitalize gallery-title">
+          {this.gallerySearch} Collection
+        </h2>
+        {isFetching ? (
+          <Spinner />
+        ) : (
+          <div className="d-flex gallery">
+            {gallery.map((movie) => (
+              <div key={movie.imdbID} className="gallery-movie">
+                <h5>{movie.Title}</h5>
+                <img src={movie.Poster} />
+              </div>
+            ))}
+          </div>
+        )}
+      </>
+    );
+  }
+}
